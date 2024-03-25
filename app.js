f00f748f1b7fcf02a55e51def125673c3535801c
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-03-25 11:09:26 +0530

    code added

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-let arr = [1, 2, 4, 8, 5];
+// let arr = [1, 2, 4, 8, 5];
 
-arr.forEach( (el) => {
-    console.log(el);
-});
+// arr.forEach( (el) => {
+//     console.log(el);
+// });
 
-let info = [ {
-    name: "biki",
-    mark: 45,
-}, {
-    name: "binay",
-    mark: 73,
-}, {
-    name: "mohanta",
-    mark: 34,
-},];
+// let info = [ {
+//     name: "biki",
+//     mark: 45,
+// }, {
+//     name: "binay",
+//     mark: 73,
+// }, {
+//     name: "mohanta",
+//     mark: 34,
+// },];
 
-info.forEach( (el) => {
-    console.log(el.mark);
+// info.forEach( (el) => {
+//     console.log(el.mark);
+// });
+
+let arr = [1, 2, 3, 4];
+
+let double = arr.map( (el) => {
+    return el * 2;
 });
